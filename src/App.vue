73c5b8f029f3f2677376cc6b93b2ad6895e1e453
LLL-zqvr <13657794496@163.com>
2024-10-05 12:22:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <h1>这里是组件的结构，，倪浩啊~</h1>
+  </div>
+</template>
+
+<script lang="ts">
+//Js 或ts
+export default {
+  name: "App", //组件名
+};
+</script>
+
+<style>
+/* 组件样式 */
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
---
+++ vite-note.md
@@ -0,0 +1,13 @@
+### vite命令
+
+#### 初始化
+
+npm install
+
+#### 运行
+
+npm run dev
+
+#### 安装所有的依赖
+
+npm i
