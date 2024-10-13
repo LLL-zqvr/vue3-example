62931c40c5c5f39319357af288886984f06ab1d5
LLL-zqvr <13657794496@163.com>
2024-10-13 11:56:47 +0800

    update

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,54 +2,39 @@
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
+    <h2>地址：{{ address }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="showTel">查看联系方式</button>
-    <hr />
-    <h2>测试:{{ a }}</h2>
-    <h3>测试2：{{ c }}</h3>
-    <button @click="test">测试</button>
   </div>
 </template>
 
-<script lang="ts">
+<!-- 这个script负责控制组件名字 -->
+<!-- <script lang="ts">
 export default {
   name: "Person",
-  data() {
-    return {
-      a: 100,
-      c: this.name,
-    };
-  },
-  methods: {
-    test() {
-      console.log("b");
-    },
-  },
-  setup() {
-    //数据原来是写在data中的，此时的name，age，tel都不是响应式的数据，不能随着值的变化，用到值的地方的值也跟着变
-    //数据
-    let name = "张三";
-    let age = 18;
-    let tel = "124243";
-    //方法
-    function changeName() {
-      name = "zhang-san"; //注意：这样修改name，页面是没有变化的
-      console.log(name); //name的值确实改了，但是name不是响应式的
-    }
-    function changeAge() {
-      age += 1; //同上
-    }
-    function showTel() {
-      alert(tel);
-    }
-    // return里前两个返回的是对象，{name:name,age:age}，只是省略了
-    //return返回的可以是对象，函数等。
-    return { name, age, changeName, changeAge, showTel };
-    //return返回的甚至可以是渲染函数，如：
-    // return () => "哈哈";这么写的话页面渲染出来的就是内容"哈哈"了，与上面的template无关了
-  },
 };
+</script> -->
+<!-- 如果没有lang="ts"，会认为上面的script中的是ts语言，而下面这个只是js语言，导致语言不统一而报错。所以要加上lang="ts"，使得下面的语句也声明为ts -->
+<!-- 下面这个加上setup既是代表写了setup(){}并且自动在大括号里面返回了所有数据方法 -->
+<!-- 这个script负责setup -->
+<script lang="ts" setup>
+//数据
+let name = "张三";
+let age = 18;
+let tel = "124243";
+let address = "东北林业大学";
+//方法
+function changeName() {
+  name = "zhang-san"; //注意：这样修改name，页面是没有变化的
+  console.log(name); //name的值确实改了，但是name不是响应式的
+}
+function changeAge() {
+  age += 1; //同上
+}
+function showTel() {
+  alert(tel);
+}
 </script>
 
 <style scoped>
--- vue3-note.md
+++ vue3-note.md
@@ -5,8 +5,9 @@
 - `Vite` 项目中，`index.html` 是项目的入口文件，在项目最外层。
 - 加载`index.html`后，`Vite` 解析 `<script type="module" src="xxx">` 指向的`JavaScript`。
 - `Vue3`**中是通过**`createApp` 函数创建一个应用实例。
-- Q: setup 和 data,methods(vue2)之间有什么关系，既 setup 和传统的配置项可不可以同时写，如果冲突了以谁为主？
+- setup 和 Options API 的关系
+  Q: setup 和 data,methods(vue2)之间有什么关系，既 setup 和传统的配置项可不可以同时写，如果冲突了以谁为主？
   A:
-- `Vue2` 的配置（`data`、`methos`......）中**可以访问到** `setup`中的属性、方法。
-- 但在`setup`中**不能访问到**`Vue2`的配置（`data`、`methos`......）。
-- 如果与`Vue2`冲突，则`setup`优先。
+  - `Vue2` 的配置（`data`、`methos`......）中**可以访问到** `setup`中的属性、方法。(因为 setup 执行度很高，早在传统配置项之前就创建出来了)
+  - 但在`setup`中**不能访问到**`Vue2`的配置（`data`、`methos`......）。
+  - 如果与`Vue2`冲突，则`setup`优先。
