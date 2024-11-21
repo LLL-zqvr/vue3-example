9d791f5989a0bb7b967117fe257d9f3170334471
LLL-zqvr <13657794496@163.com>
2024-11-21 22:16:22 +0800

    update

--- backups/16_ts中的接口，自定义类型，泛型/src/components/Person.vue
+++ backups/16_ts中的接口，自定义类型，泛型/src/components/Person.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="person"></div>
+  <div class="person">？？？</div>
 </template>
 
 <script lang="ts" setup name="Person">
 // 这里的@是指一下跳到最顶的路径上了
 //声明引入接口时前面要加上type，以和普通的暴露出的值区分开
-import { type PersonInter, type Persons } from "@/types";
+import { type PersonInter, type Persons } from "../types/index";
 
 //这里是指，定义一个person，但改person必须符合接口规范
 let person: PersonInter = { id: "sdfsa", name: "张三", age: 60 };
